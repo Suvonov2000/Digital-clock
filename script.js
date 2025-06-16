f3969c711c96b8adecdf9cf69845f349920403be
Suvonov2000 <suvonov200@gmail.com>
2025-06-16 09:42:11 +0900

    digital clock

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
-window.addEventListener (`load`,calculateTime)
+window.addEventListener(`load`, calculateTime);
 
-function calculateTime(){
-    var date = new Date();
-    var dayNumber = date.getDate();
-    var hour = date.getHours();
-    var minute = date.getMinutes();
-    var  ampm = hour >= 12 ? `PM` : `AM`;
-    var dayNames = ["SUN","MON","TUE","WED","THU","FRI","SAT"];
+function calculateTime() {
+  var date = new Date();
 
-    hour = hour % 12;
-    hour = hour ? 12 : `12`;
-    hour = hour < 10 ? `0` + hour : hour;
-    minute = minute < 10 ? `0` + minute : minute;
+  var utc = date.getTime() + date.getTimezoneOffset() * 60000;
+  var japanDate = new Date(utc + 9 * 60 * 60000); // JST = UTC + 9
 
-    document.getElementById("day").innerHTML=dayNames[dayNumber];
-    document.getElementById("hour").innerHTML = hour;
-    document.getElementById("minute").innerHTML = minute;
-    document.getElementById("ampm").innerHTML = ampm;
+  var dayNumber = japanDate.getDay();
+  var hour = japanDate.getHours();
+  var minute = japanDate.getMinutes();
+  var ampm = hour >= 12 ? "PM" : "AM";
+  var dayNames = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
 
-    setTimeout(calculateTime,200)
+  hour = hour % 12;
+  hour = hour ? hour : 12;
+  hour = hour < 10 ? "0" + hour : hour;
+  minute = minute < 10 ? "0" + minute : minute;
+
+  document.getElementById("day").innerHTML = dayNames[dayNumber];
+  document.getElementById("hour").innerHTML = hour;
+  document.getElementById("minute").innerHTML = minute;
+  document.getElementById("ampm").innerHTML = ampm;
+
+  setTimeout(calculateTime, 1000);
 }
